09499bad23224970988f59240a7326cda34e9aaa
marjus <marjus@hotmail.com>
2023-01-10 08:40:09 +0100

    Added connectionstring

--- ReceptMT.API/Program.cs
+++ ReceptMT.API/Program.cs
@@ -8,7 +8,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddDbContext<RecipeContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("AzureConnection")));
     //options.UseInMemoryDatabase("RecipeList"));
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
